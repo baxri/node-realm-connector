706f7c8142cbb90675008ef003af6c6b40e1f73f
giorgi <giorgi.bibilashvili89@gmail.com>
2018-08-13 17:40:25 +0400

    --

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+var Realm = require('realm');
+var express = require('express');
+
+const app = express();
+
+const realmPath = './DB/haccp-db-8.realm';
 
 
-console.log("OK");
+let realm = new Realm({
+    path: realmPath,
+});
+
+app.get('/', function (req, res) {
+    let items = realm.objects(req.query.document);
+    res.json(items);
+});
+
+app.listen(8080, function () {
+    console.log('App listening on port 8080!');
+});
+
+console.log(app);
+
+
+
