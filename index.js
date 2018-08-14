d575fac44d19b58cbee76826fd0909ec9d9c8baa
giorgi <giorgi.bibilashvili89@gmail.com>
2018-08-14 20:52:25 +0200

    --

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 var Realm = require('realm');
 var express = require('express');
-
 const app = express();
-
 const realmPath = './DB/haccp-db-8.realm';
-
 
 let realm = new Realm({
     path: realmPath,
 });
 
 app.get('/', function (req, res) {
-
-    
-
     let result = realm.objects(req.query.document);
     if (req.query.filter.length > 0 && req.query.filter_args.length > 0) {
-        let filter = req.query.filter.replace("/", "=");
-
+        let filter = req.query.filter.replace('/', '=');
         res.json(filter);
-
         result = result.filtered(...[filter, ...req.query.filter_args]);
     }
 
